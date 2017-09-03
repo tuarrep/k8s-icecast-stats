96700e03cf92eb38fe39b29fd7e6b4ab12cb4026
Nicolas Perraut <n.perraut@gmail.com>
2017-09-03 17:15:17 +0200

    Allow config via env vars

--- aggregator.js
+++ aggregator.js
@@ -1,10 +1,66 @@
+const fs = require('fs')
+
 const k8s = require('./modules/k8s')
 const podPool = require('./modules/pod-pool')
 const statsParser = require('./modules/stats-parser')
 
-const config = require('./config')
+const buildConfig = () => {
+  let config = {}
 
-k8s.getIcecastPods(config.k8s.namespace, podPool.handlePodsEvents);
+  if (fs.existsSync('./config.json')) {
+    config = require('./config')
+    console.log(config)
+  } else {
+    console.log('I: Config file not found. Trying to build config from env vars')
+  }
+
+  if (config.k8s == undefined) {
+    config.k8s = {}
+  }
+
+  if (process.env.MONITOR_NAMESPACE) {
+    config.k8s.namespace = process.env.MONITOR_NAMESPACE
+  } else if (config.k8s.namespace == undefined) {
+    config.k8s.namespace = 'default'
+  }
+
+  if (process.env.IN_CLUSTER) {
+    config.k8s.inCluster = process.env.IN_CLUSTER == 'true' ? true : false
+  } else if (config.k8s.inCluster == undefined) {
+    console.error('E: I need to know if I\'m in a K8s cluster or not. Neither IN_CLUSTER env var nor config.k8s.inCluster is defined. Aborting')
+    return false;
+  }
+
+  if (config.k8s.apiConfig == undefined) {
+    config.k8s.apiConfig = {}
+  }
+
+  if (process.env.API_ENDPOINT) {
+    config.k8s.apiConfig.endpoint = process.env.API_ENDPOINT
+  } else if (config.k8s.apiConfig.endpoint == undefined) {
+    config.k8s.apiConfig.endpoint = config.k8s.inCluster ? 'https://' + process.env.KUBERNETES_SERVICE_HOST + ':' + process.env.KUBERNETES_PORT_443_TCP_PORT : 'http://localhost:8001'
+  }
+
+  if(process.env.API_VERSION) {
+    config.k8s.apiConfig.version = process.env.API_VERSION
+  } else if (config.k8s.apiConfig.version == undefined) {
+    config.k8s.apiConfig.version = '/api/v1'
+  }
+
+  return config
+}
+
+const config = buildConfig()
+
+console.log(config)
+
+if (!config) {
+  process.exit(1)
+}
+
+k8s.init(config)
+
+k8s.getIcecastPods(config.k8s.namespace, podPool.handlePodsEvents)
 
 setInterval(() => {
   statsParser.getStats(podPool.getPool())
--- modules/k8s.js
+++ modules/k8s.js
@@ -1,28 +1,34 @@
 const k8s = require('k8s')
 const fs = require('fs')
-const config = require('../config')
 
-let apiConfig = config.k8s.apiConfig;
-let auth = {}
-
-if (config.k8s.inCluster) {
-  auth = {
-    token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token')
-  }
-} else {
-  auth = {
-    clientKey: fs.readFileSync('keys/apiserver.key'),
-    clientCert: fs.readFileSync('keys/apiserver.crt'),
-    caCert: fs.readFileSync('keys/ca.crt')
-  }
-}
-
-apiConfig.auth = auth
-
-const kubeapi = k8s.api(apiConfig)
+let kubeapi = null
 
 module.exports = {
+  init: (config) => {
+    let apiConfig = config.k8s.apiConfig;
+    let auth = {}
+
+    if (config.k8s.inCluster) {
+      auth = {
+        token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token')
+      }
+    } else {
+      auth = {
+        clientKey: fs.readFileSync('keys/apiserver.key'),
+        clientCert: fs.readFileSync('keys/apiserver.crt'),
+        caCert: fs.readFileSync('keys/ca.crt')
+      }
+    }
+
+    apiConfig.auth = auth
+
+    kubeapi = k8s.api(apiConfig)
+  },
   getIcecastPods: (namespace, callback) => {
+    if(kubeapi == null) {
+      console.error('E: K8s client isn\'t defined! Did you forgot to call init()? ')
+      process.exit(2)
+    }
     kubeapi.watch('watch/namespaces/' + namespace + '/pods').subscribe(
       data => {
         callback(false, data)
