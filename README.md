7abd5579c70d5c3c2a23ccaadb493959af132557
Nicolas Perraut <n.perraut@gmail.com>
2017-09-06 20:29:15 +0000

    parse and process stats

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ npm install icecast-stats --save
 ## Dependencies
 
 - [k8s](https://github.com/goyoo/node-k8s-client): Node.js client library for Google&#39;s Kubernetes Kubectl And API
+- [xml2js](https://github.com/Leonidas-from-XIV/node-xml2js): Simple XML to JavaScript object converter.
 
 ## Dev Dependencies
 
--- modules/stats-parser.js
+++ modules/stats-parser.js
@@ -1,10 +1,31 @@
 const http = require('http')
+const parseString = require('xml2js').parseString
+
+const parseStatsData = (data) => {
+  let listeners = []
+  parseString(data, (err, json) => {
+    if (err) {
+      console.error('E: ' + err);
+      []
+    }
+
+    json.icestats.source.forEach((source) => {
+      const name = source.$.mount
+      const listenersCount = parseInt(source.listeners[0])
+
+      listeners[name] = listenersCount
+    })
+  })
+  return listeners
+}
 
 module.exports = {
   getStats: (podPool) => {
+    let listeners = [];
+
     let password = 'hackme'
 
-    if(process.env.ADMIN_PASSWORD) {
+    if (process.env.ADMIN_PASSWORD) {
       password = process.env.ADMIN_PASSWORD
     }
 
@@ -25,10 +46,20 @@ module.exports = {
             body += d
           });
           response.on('end', function() {
-            console.log(body)
+            const podListeners = parseStatsData(body)
+
+            podListeners.forEach((count, source) => {
+              if (listeners[source] !== undefined) {
+                listeners[source] = count
+              } else {
+                listeners[source] += count
+              }
+            })
           })
         })
       }
     }
+
+    console.log(listeners)
   }
 }
