0013c5cccbdb0f5184549bba3422b57e41a47c00
Nicolas Perraut <n.perraut@gmail.com>
2017-09-06 20:21:34 +0000

    Get raw XML stats from each pods

--- modules/stats-parser.js
+++ modules/stats-parser.js
@@ -2,11 +2,32 @@ const http = require('http')
 
 module.exports = {
   getStats: (podPool) => {
-    for(client in podPool) {
+    let password = 'hackme'
+
+    if(process.env.ADMIN_PASSWORD) {
+      password = process.env.ADMIN_PASSWORD
+    }
+
+    for (client in podPool) {
       const pods = podPool[client]
       for (podName in pods) {
         const pod = pods[podName]
-        console.log(pod)
+        http.get({
+          host: pod.ip,
+          port: 8000,
+          path: '/admin/stats',
+          headers: {
+            'Authorization': 'Basic ' + new Buffer('admin:' + password).toString('base64')
+          }
+        }, (response) => {
+          var body = ''
+          response.on('data', function(d) {
+            body += d
+          });
+          response.on('end', function() {
+            console.log(body)
+          })
+        })
       }
     }
   }
